25fb4f80c6fb26a7cbd0f1d14f2e08d5b5eb10a0
Jacek Konieczny <jajcus@jajcus.net>
2002-01-30 16:52:03 +0000

    - info about licensing and RCS Id tag in header of each source file

--- src/register.c
+++ src/register.c
@@ -1,3 +1,22 @@
+/* $Id: register.c,v 1.11 2002/01/30 16:52:03 jajcus Exp $ */
+
+/*
+ *  (C) Copyright 2002 Jacek Konieczny <jajcus@pld.org.pl>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include "ggtrans.h"
 #include "jabber.h"
 #include "register.h"
