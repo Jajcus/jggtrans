aa11b3d9995a2ee5b05887c078ac53d9b31f1a94
Jacek Konieczny <jajcus@jajcus.net>
2004-10-28 19:31:03 +0000

    - 'ChangeLog' and 'cosmetics' make targets - auto* tools update

--- aux/cosmetics.sh
+++ aux/cosmetics.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+dir="`dirname $0`"
+
 find . '(' -name "*.c" -o -name "*.h" ')' -a '(' '!' -path './libxode/*' -a '!' -path './intl/*' ')' \
-	| xargs -n1 vim -u NONE -s cosmetics.vim 
+	| xargs -n1 vim -u NONE -s "$dir/cosmetics.vim" 
 stty sane
