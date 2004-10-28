56271bea7e530be761d1c1c9b729fc24a77f3b30
Jacek Konieczny <jajcus@jajcus.net>
2004-10-28 16:07:57 +0000

    - nicer tag names

--- cosmetics.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find . '(' -name "*.c" -o -name "*.h" ')' -a '(' '!' -path './libxode/*' -a '!' -path './intl/*' ')' \
-	| xargs -n1 vim -u NONE -s cosmetics.vim 
-stty sane
--- cosmetics.vim
+++
@@ -1,9 +0,0 @@
-:set tabwidth=8
-:set nocompatible
-:%s/\s\+$//
-:%s/)\s*\(\_$\|\s\+\)\_\s*{/){/g
-:%s/else\s*\(\_$\|\s\+\)\_\s*{/else{/g
-:retab!
-:update
-:q
-:q!
